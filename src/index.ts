6ad47f5de285e2b69169e847eea9b2e3a69e8d3b
Piyush Bhangale <bhangalepiyush@gmail.com>
2022-04-15 22:49:36 +0530

    change folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 require("dotenv").config();
-import { PrismaClient } from "@prisma/client";
 import express from "express";
 
-const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
