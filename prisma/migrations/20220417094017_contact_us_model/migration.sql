4a67f273f39b48d966a42410d16ae092b2b35c4e
Piyush Bhangale <bhangalepiyush@gmail.com>
2022-04-17 15:11:06 +0530

    feat: contact us form

---
+++ prisma/migrations/20220417094017_contact_us_model/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "ContactRequests" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ContactRequests_pkey" PRIMARY KEY ("id")
+);
--- src/routes/FormRoute.ts
+++ src/routes/FormRoute.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import prisma from "../lib/prisma";
+import sendMessage from "../lib/twilio/sendMessage";
 
 const router = Router();
 
@@ -46,6 +47,33 @@ router.post("/donate", async (req, res) => {
         amount,
       },
     });
+
+    res.json({ success: true });
+  } catch (error: any) {
+    console.log(error);
+    res.status(500).json({ success: false, error: error.message });
+    return;
+  }
+});
+
+router.post("/contact", async (req, res) => {
+  const { name, email, message } = req.body;
+
+  console.log(req.body);
+
+  try {
+    await prisma.contactRequests.create({
+      data: {
+        name,
+        email,
+        message,
+      },
+    });
+
+    await sendMessage(
+      `New contact request from ${name} (${email})\n\n${message}`,
+      process.env.ADMIN_MOBILE_NUMBER!!
+    );
 
     res.json({ success: true });
   } catch (error: any) {
