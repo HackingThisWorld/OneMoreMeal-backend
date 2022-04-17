4636b40071e0582967089380f9fd0d4a8c649465
Piyush Bhangale <bhangalepiyush@gmail.com>
2022-04-17 12:51:38 +0530

    feat: new routes

--- src/routes/FormRoute.ts
+++ src/routes/FormRoute.ts
@@ -3,18 +3,41 @@ import prisma from "../lib/prisma";
 
 const router = Router();
 
-router.post("/", async (req, res) => {
+router.post("/requests", async (req, res) => {
   const { name, shop_name, number, address, time, note } = req.body;
 
+  const pickupDate = new Date(time);
+
   try {
-    await prisma.form.create({
+    await prisma.requests.create({
       data: {
         name,
         shop_name,
         number,
         address,
-        time,
         note,
+        pickup_time: pickupDate,
+      },
+    });
+
+    res.json({ success: true });
+  } catch (error: any) {
+    console.log(error);
+    res.status(500).json({ success: false, error: error.message });
+    return;
+  }
+});
+
+router.post("/donate", async (req, res) => {
+  const { name, email, contact_number, amount } = req.body;
+
+  try {
+    await prisma.donation.create({
+      data: {
+        name,
+        email,
+        contact_number,
+        amount,
       },
     });
 
